f1b7049e6d2af9d87e6a90e7b8980e854cebe4ba
GabrielMartins <102445948+GabriellMartins@users.noreply.github.com>
2024-07-26 19:22:39 -0300

    create redis connection

---
+++ src/main/java/br/com/skywars/core/Constants.java
@@ -0,0 +1,8 @@
+package br.com.skywars.core;
+
+public class Constants {
+
+    public final static String REDIS_PORT = "6379";
+    public final static String REDIS_PASSWORD = "";
+    public final static String REDIS_DATABASE = "localhost";
+}
